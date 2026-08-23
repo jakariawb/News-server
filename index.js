6fc1c5d44cbacc6b2c8fc204fef43b256f869e18
jakaria <jakarija043@gmail.com>
2026-08-23 08:47:29 +0600

    first commit

--- index.js
+++ index.js
@@ -89,6 +89,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+export default app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
